f0ba623b9d6083ea1fe9319678a06f3df3f4b6d8
Chris Rush <chris@crrush.com>
2016-06-07 08:03:07 -0600

    Missing file.

---
+++ pre-cu/src/main/java/com/ocdsoft/bacta/swg/conf/ConfigSections.java
@@ -0,0 +1,12 @@
+package com.ocdsoft.bacta.swg.conf;
+
+/**
+ * Created by crush on 6/6/2016.
+ */
+public final class ConfigSections {
+    public static final String GAME_SERVER = "Bacta/GameServer";
+    public static final String GAME_SERVER_UNIVERSE = GAME_SERVER + "/Universe";
+    public static final String GAME_SERVER_SCENES = GAME_SERVER + "/Scenes";
+
+    public static final String GAME_SERVER_SCRIPTS = GAME_SERVER + "/Scripts";
+}
