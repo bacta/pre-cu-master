b2431214f8110dd43b924bcd9640a01f7e7c2a03
Adrian Medina <adrian.medina@mail.yu.edu>
2016-06-08 17:11:29 -0400

    add deps

--- scripts/src/main/clojure/bacta/core.clj
+++ scripts/src/main/clojure/bacta/core.clj
@@ -1,11 +1,12 @@
 (ns bacta.core
   (:import [com.ocdsoft.bacta.soe.event Event]
            [com.ocdsoft.bacta.swg.server.game.script ScriptService]
-           [com.ocdsoft.bacta.soe.service PublisherService]))
+           [com.ocdsoft.bacta.soe.service PublisherService]
+           [java.util.function Consumer]))
 
-(defonce ^ScriptService script-service nil)
+;; (defonce ^ScriptService script-service nil)
 
-(defonce ^PublisherService publisher-service nil)
+;; (defonce ^PublisherService publisher-service nil)
 
 (defn subscribe
   "Subscribes callback function `f` to event."
