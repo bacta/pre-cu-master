ac915f3f837bb526b233d9d48fd4249246500b9b
Adrian Medina <adrian.medina@mail.yu.edu>
2016-06-08 17:06:56 -0400

    Consumer -> clojure.lang.IFn

--- scripts/src/main/clojure/bacta/core.clj
+++ scripts/src/main/clojure/bacta/core.clj
@@ -1,10 +1,14 @@
 (ns bacta.core
-  (:import com.ocdsoft.bacta.soe.event.Event))
+  (:import [com.ocdsoft.bacta.soe.event Event]
+           [com.ocdsoft.bacta.swg.server.game.script ScriptService]
+           [com.ocdsoft.bacta.soe.service PublisherService]))
 
-(defonce script-service nil)
+(defonce ^ScriptService script-service nil)
+
+(defonce ^PublisherService publisher-service nil)
 
 (defn subscribe
   "Subscribes callback function `f` to event."
-  [event f]
-  )
+  [^Event event f]
+  (.subscribe publisher-service event f))
 
--- soe/src/main/java/com/ocdsoft/bacta/soe/service/PublisherService.java
+++ soe/src/main/java/com/ocdsoft/bacta/soe/service/PublisherService.java
@@ -7,7 +7,8 @@ import com.ocdsoft.bacta.soe.message.GameNetworkMessage;
 import com.ocdsoft.bacta.soe.message.Subscribable;
 
 import java.util.*;
-import java.util.function.Consumer;
+
+import clojure.lang.IFn;
 
 /**
  * Created by kyle on 5/21/2016.
@@ -15,41 +16,41 @@ import java.util.function.Consumer;
 @Singleton
 public class PublisherService {
 
-    private final Map<Class, Set<Consumer>> eventConsumers;
+    private final Map<Class, Set<IFn>> eventCallbacks;
 
     public PublisherService() {
-        eventConsumers = Collections.synchronizedMap(new HashMap<>());
+        eventCallbacks = Collections.synchronizedMap(new HashMap<>());
     }
 
-    public final <T extends Event> boolean subscribe(Class<T> eventClass, Consumer<T> handleMethod) {
+    public final <T extends Event> boolean subscribe(Class<T> eventClass, IFn handleMethod) {
 
-        Set<Consumer> consumers = eventConsumers.get(eventClass);
-        if(consumers == null) {
-            consumers = Collections.synchronizedSet(new HashSet<>());
-            eventConsumers.put(eventClass, consumers);
+        Set<IFn> callbacks = eventCallbacks.get(eventClass);
+        if(callbacks == null) {
+            callbacks = Collections.synchronizedSet(new HashSet<>());
+            eventCallbacks.put(eventClass, callbacks);
         }
 
-        return consumers.add(handleMethod);
+        return callbacks.add(handleMethod);
     }
 
-    public final <T extends Event> boolean unsubscribe(Class<T> eventClass, Consumer<T> handleMethod) {
-        Set<Consumer> consumers = eventConsumers.get(eventClass);
-        if (consumers == null) {
+    public final <T extends Event> boolean unsubscribe(Class<T> eventClass, IFn handleMethod) {
+        Set<IFn> callbacks = eventCallbacks.get(eventClass);
+        if (callbacks == null) {
             return true;
         }
 
-        boolean result = consumers.remove(handleMethod);
-        if(consumers.isEmpty()) {
-            eventConsumers.remove(eventClass);
+        boolean result = callbacks.remove(handleMethod);
+        if(callbacks.isEmpty()) {
+            eventCallbacks.remove(eventClass);
         }
         return result;
     }
 
     public final <T extends Event> void onEvent(final T event) {
-        final Set<Consumer> consumers = eventConsumers.get(event.getClass());
-        if(consumers != null) {
-            for(Consumer consumer : consumers) {
-                consumer.accept(event);
+        final Set<IFn> callbacks = eventCallbacks.get(event.getClass());
+        if(callbacks != null) {
+            for(IFn f : callbacks) {
+                f.invoke(event);
             }
         }
     }
