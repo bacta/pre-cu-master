e2dac396b0291a1503779cea96e1a12101fcbc21
Adrian Medina <adrian.medina@mail.yu.edu>
2016-06-07 21:23:33 -0400

    print arguments to verify call

--- scripts/main/clojure/bacta/player/base/base_player.clj
+++ scripts/main/clojure/bacta/player/base/base_player.clj
@@ -1,10 +1,13 @@
 (ns bacta.player.base.base-player
   (:use bacta.core)
-  (:import [com.ocdsoft.bacta.soe.event ConnectEvent]))
+  (:import [com.ocdsoft.bacta.soe.event ConnectEvent]
+           [com.ocdsoft.bacta.swg.server.game.object ServerObject]))
 
 (defn on-login
-  [^ConnectEvent event]
+  [^ConnectEvent event ^ServerObject object]
   (let [connection (.-connection event)]
-    ))
+    (println :event event
+             :object object
+             :connection connection)))
 
 (subscribe ConnectEvent on-login)
