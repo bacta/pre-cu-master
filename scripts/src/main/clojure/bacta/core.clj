e775902a48562d98e94454078e500f0029e83b98
Adrian Medina <adrian.medina@mail.yu.edu>
2016-06-08 17:16:54 -0400

    consume event through publisher-service

--- scripts/src/main/clojure/bacta/core.clj
+++ scripts/src/main/clojure/bacta/core.clj
@@ -4,12 +4,16 @@
            [com.ocdsoft.bacta.soe.service PublisherService]
            [java.util.function Consumer]))
 
-;; (defonce ^ScriptService script-service nil)
+(defonce ^ScriptService script-service nil)
 
-;; (defonce ^PublisherService publisher-service nil)
+(defonce ^PublisherService publisher-service nil)
 
 (defn subscribe
   "Subscribes callback function `f` to event."
   [^Event event f]
-  (.subscribe publisher-service event f))
+  (.subscribe publisher-service
+              event
+              (reify Consumer
+                (accept [this event]
+                  (f event)))))
 
