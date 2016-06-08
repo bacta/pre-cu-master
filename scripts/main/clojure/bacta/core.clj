be2b8086158bd1b7aa1e425acdb703c2530aeb0a
Adrian Medina <adrian.medina@mail.yu.edu>
2016-06-07 21:36:54 -0400

    declare script-service var

--- scripts/main/clojure/bacta/core.clj
+++ scripts/main/clojure/bacta/core.clj
@@ -1,7 +1,10 @@
 (ns bacta.core
   (:import com.ocdsoft.bacta.soe.event.Event))
 
+(defonce script-service nil)
+
 (defn subscribe
   "Subscribes callback function `f` to event."
-  [event f])
+  [event f]
+  )
 
